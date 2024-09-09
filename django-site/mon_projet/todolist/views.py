fc3f92dfba054b048d6e67a3b6684e7c30546979
fatim <fatim.mbodjfatim14@gmail.com>
2024-09-09 19:56:01 +0200

    site bien en forme

--- django-site/mon_projet/todolist/views.py
+++ django-site/mon_projet/todolist/views.py
@@ -1,10 +1,24 @@
 from .forms import TaskForm
-
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
+
 def task_list(request):
-    tasks = Task.objects.all()
+    # Initialisation des paramètres de tri et de filtrage
+    sort_by = request.GET.get('sort', 'created_at')  # Tri par défaut par date de création
+    status_filter = request.GET.get('status', None)  # Pas de filtre par défaut
+
+    # Récupération des tâches avec filtrage optionnel
+    if status_filter == 'done':
+        tasks = Task.objects.filter(status=True)
+    elif status_filter == 'todo':
+        tasks = Task.objects.filter(status=False)
+    else:
+        tasks = Task.objects.all()
+
+    # Application du tri
+    tasks = tasks.order_by(sort_by)
+
     return render(request, 'todolist/task_list.html', {'tasks': tasks})
 
 def task_add(request):
